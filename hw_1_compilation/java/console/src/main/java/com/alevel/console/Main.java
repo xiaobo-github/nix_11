7224c197da1b5f8f596e38a3b340c2cec53a5934
orakcool <realjmail@mail.ru>
2022-02-08 13:55:05 +0200

    Test commit compile with jar

---
+++ hw_1_compilation/java/console/src/main/java/com/alevel/console/Main.java
@@ -0,0 +1,9 @@
+package com.alevel.console;
+import com.alevel.console.run.SayToConsole;
+
+public class Main{
+    public static void main(String[] args){
+            SayToConsole sayToConsole = new SayToConsole();
+            sayToConsole.say();
+        }
+}
---
+++ hw_1_compilation/java/console/src/main/java/com/alevel/console/run/SayToConsole.java
@@ -0,0 +1,8 @@
+package com.alevel.console.run;
+import org.apache.commons.lang3.*;
+
+public class SayToConsole{
+           public void say(){
+               System.out.println(StringUtils.rotate("!dlroW olleH",0));
+        }
+}
