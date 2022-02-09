07d05e247f3ac75c895fe6dd2dec75b69ae85c71
orakcool <realjmail@mail.ru>
2022-02-09 07:51:06 +0200

    Test commit compile with jar

--- hw_1_compilation/java/console/src/main/java/com/alevel/console/run/SayToConsole.java
+++ hw_1_compilation/java/console/src/main/java/com/alevel/console/run/SayToConsole.java
@@ -4,6 +4,6 @@ import org.apache.commons.lang3.*;
 
 public class SayToConsole{
            public void say(){
-               System.out.println(StringUtils.rotate("!dlroW olleH",0));
+               System.out.println(StringUtils.reverse("!dlroW olleH"));
         }
 }
