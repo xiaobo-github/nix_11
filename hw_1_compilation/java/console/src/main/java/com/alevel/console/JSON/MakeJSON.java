041df8a34f20e971e469f354b32825fb1f3797ec
orakcool <realjmail@mail.ru>
2022-02-10 22:03:18 +0200

    Test commit compile with jar

---
+++ hw_1_compilation/java/console/src/main/java/com/alevel/console/JSON/MakeJSON.java
@@ -0,0 +1,11 @@
+package com.alevel.console.JSON;
+
+import com.alibaba.fastjson.JSON;
+
+public class MakeJSON{
+    public static void make(String string){
+        System.out.println("Origin string:");
+        System.out.println(string);
+        System.out.print("Reverse string:");
+    }
+}
