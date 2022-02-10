fa4de6780a2f3219bede46e8a33fdf4eca3ea77a
orakcool <realjmail@mail.ru>
2022-02-10 18:15:31 +0200

    Test commit compile with jar

--- hw_1_compilation/java/console/build.sh
+++ hw_1_compilation/java/console/build.sh
@@ -1,21 +1,19 @@
 #!/bin/sh
 
-#go to main directory
-#cd ..
 
 #repeat part of path
-#console_source_path="src/main/java/"
+sp="src/main/java/"
 #path to libs
-#libs="libs/commons-lang3-3.11.jar"
+libs="./libs/commons-lang3-3.11.jar"
 
 
 #compile
 echo "Compile..."
-javac -sourcepath src/main/java/ -d build/classes/ -cp ./libs/commons-lang3-3.11.jar src/main/java/com/alevel/console/Main.java
+javac -sourcepath ${sp} -d build/classes/ -cp ${libs} ${sp}com/alevel/console/Main.java
 
 
 #run
 echo "Start program.."
 echo ""
 
-java -cp build/classes:./libs/commons-lang3-3.11.jar com.alevel.console.Main
+java -cp build/classes:${libs} com.alevel.console.Main
